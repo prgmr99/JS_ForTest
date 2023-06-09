cfc3072b4d1c42c44c5280862a9902154466a295
prgmr99 <johnyeom24@naver.com>
2023-06-09 11:37:02 +0900

    링버퍼

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -1,90 +1,88 @@
 #include <iostream>
-#include <thread>
-#include <algorithm>
-#include <mutex>
-#include <chrono>
 #include <vector>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <algorithm>
 
-#define RING_SIZE 20
-#define NEXT(index) ((index+1) % RING_SIZE)
+class RingBuffer {
+public:
+    RingBuffer(size_t size) : buffer(size), read_ptr(0), write_ptr(0) {}
 
-using namespace std;
-
-typedef struct Ring{
-    int buf[RING_SIZE];
-    int head;
-    int tail;
-} Ring;
-
-// Initializa Ring
-void InitRing(Ring *ring) {
-    ring->head = ring->tail = 0;
-}
-
-// Check full
-int IsFull(Ring *ring) {
-    return NEXT(ring->tail) == ring->head;
-}
-
-// Check empty
-int IsEmpty(Ring *ring) {
-    return ring->head == ring->tail;
-}
-
-// Put data in ring
-void putData(Ring *ring, int data, mutex& m) {
-    m.lock();
-    this_thread::sleep_for(chrono::milliseconds(100));
-    if(IsFull(ring)) {
-        cout << "Ring is full" << '\n';
-        ring->head = 0;
-    }
-    ring->buf[ring->head] = data;
-    ring->head = NEXT(ring->head);
-    m.unlock();
-}
-
-// Get data from ring
-void getData(Ring *ring, mutex& m) {
-    m.lock();
-    this_thread::sleep_for(chrono::milliseconds(100));
-    int temp = 0;
-    if(IsEmpty(ring)) {
-        cout << "Ring is empty" << '\n';
-    }
-    temp = ring->buf[ring->tail];
-    ring->tail = NEXT(ring->tail);
-    m.unlock();
-}
-
-int main(void) {
-    cout << "Program Start" << '\n';
-
-    // Ring 객체 생성
-    Ring ring;
-
-    // Mutex 설정
-    mutex m;
-    int num;
-
-    cout << "숫자를 입력하세요: " << '\n';
-    cin >> num;
-
-    // 링버퍼 초기화
-    InitRing(&ring);
-    
-    // Thread -> Producer, Consumer 생성
-    while(true) {
-        if(IsFull(&ring) != 0) 
-            break;
-        thread producer(putData, &ring, num, ref(m));
-        thread consumer(getData, &ring, ref(m));
-
-        producer.join();
-        consumer.join();
+    bool push(int data) {
+        std::unique_lock<std::mutex> lock(mutex);
+        size_t next_write_ptr = (write_ptr + 1) % buffer.size();
+        if(next_write_ptr == read_ptr) {
+            return false;
+        }
+        buffer[write_ptr] = data;
+        write_ptr = next_write_ptr;
+        return true;
     }
 
-    
-    return 0;
+    int pop() {
+        std::unique_lock<std::mutex> lock(mutex);
+        if(read_ptr == write_ptr) {
+            return -1;
+        }
+        int result = buffer[read_ptr];
+        read_ptr = (read_ptr + 1) % buffer.size();
+        return result;
+    }
+
+private:
+    std::vector<int> buffer;
+    size_t read_ptr, write_ptr;
+    std::mutex mutex;
+};
+
+void data_generator(RingBuffer &buffer, size_t max_count, double mean, double stddev) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<> d(mean, stddev);
+
+    for(size_t i = 0; i < max_count; i++) {
+        size_t sleep_time = std::round(std::max(0., d(gen)));
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+        if(buffer.push(i)) {
+            std::cout << "Generated data: " << i << std::endl;
+        } else {
+            std::cout << "Buffer full, lost data: " << i << std::endl;
+        }
+    }
 }
 
+void data_processor(RingBuffer &buffer, size_t max_count, double mean, double stddev) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<> d(mean, stddev);
+
+    for(size_t i = 0; i < max_count; i++) {
+        size_t sleep_time = std::round(std::max(0., d(gen)));
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+        int data = buffer.pop();
+        if(data >= 0) {
+            std::cout << "Processed data: " << data << std::endl;
+        } else {
+            std::cout << "Buffer empty, waiting for data" << std::endl;
+        }
+    }
+}
+
+int main() {
+    RingBuffer buffer(10);
+
+    double data_gen_mean = 30;
+    double data_gen_stddev = 5;
+    double data_proc_mean = 25;
+    double data_proc_stddev = 5;
+
+    size_t num_data_points = 100;
+
+    std::thread generator(data_generator, std::ref(buffer), num_data_points, data_gen_mean, data_gen_stddev);
+    std::thread processor(data_processor, std::ref(buffer), num_data_points, data_proc_mean, data_proc_stddev);
+
+    generator.join();
+    processor.join();
+}
