e1b6e56732635770833aafdd195a5f85c8d55b44
prgmr99 <johnyeom24@naver.com>
2023-12-27 22:37:15 +0900

    다른 풀이 추가

--- finiteSosoo.js
+++ finiteSosoo.js
@@ -4,3 +4,32 @@ function solution(a, b) {
   // 아니라면 무한소수이므로 2를 리턴한다.
   return Number((a / b).toFixed(10)) == a / b ? 1 : 2;
 }
+
+// 최대 공약수 구하기
+function cal_gcd(x, y) {
+  return x % y === 0 ? y : cal_gcd(y, x % y);
+}
+
+// 소인수분해 하기
+function primeFactors(n) {
+  let result = [];
+  let divisor = 2;
+
+  while (n >= 2) {
+    if (n % divisor === 0) {
+      result.push(divisor);
+      n = n / divisor;
+    } else divisor++;
+  }
+
+  return [...new Set(result)];
+}
+
+function solution(a, b) {
+  const gcd = cal_gcd(a, b);
+  a = a / gcd;
+  b = b / gcd;
+
+  const primes = primeFactors(b);
+  return primes.find((n) => n !== 2 && n !== 5) ? 2 : 1;
+}
