fe88efd41158da4250ff10336560b5a76f1f2b8f
prgmr99 <johnyeom24@naver.com>
2024-01-16 09:54:48 +0900

    핸드폰 번호 가리기

---
+++ HidePhoneNum.js
@@ -0,0 +1,8 @@
+function solution(phone_number) {
+  // 전화번호의 뒷 4자리를 제외한 부분을 *로 대체
+  const hiddenPart = "*".repeat(phone_number.length - 4);
+  // 뒷 4자리를 가져와서 뒤에 이어붙임
+  const visiblePart = phone_number.slice(-4);
+
+  return hiddenPart + visiblePart;
+}
