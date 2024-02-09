48030e6676682990ca05a34d74898bc22d4b9834
prgmr99 <johnyeom24@naver.com>
2024-02-09 10:01:36 +0900

    소수찾기

---
+++ FindSosu.js
@@ -0,0 +1,22 @@
+function solution(n) {
+  let answer = 0;
+  const arr = new Array(n + 1).fill(true);
+
+  for (let i = 2; i <= n; ++i) {
+    // 이미 소수가 아닌 인덱스는 건너뛴다.
+    if (arr[i] === false) {
+      continue;
+    }
+    // 배수는 소수가 아니라 0으로 설정
+    for (let k = i * 2; k <= n; k += i) {
+      arr[k] = false;
+    }
+  }
+  // 소수의 갯수를 구한다.
+  for (let i = 2; i <= n; ++i) {
+    if (arr[i] === true) {
+      answer++;
+    }
+  }
+  return answer;
+}
