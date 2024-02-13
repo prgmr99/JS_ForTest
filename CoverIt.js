4d5ee548f0ce6a7b5adb2d09d5aa5bb1abe6fe2b
prgmr99 <johnyeom24@naver.com>
2024-02-13 10:36:26 +0900

    덧칠하기

--- CoverIt.js
+++ CoverIt.js
@@ -1,5 +1,20 @@
 function solution(n, m, section) {
-  const temp = section[section.length - 1] - section[0] + 1;
+  let answer = 0;
 
-  return Math.ceil(temp / m);
+  // 현재까지 칠한 구역
+  let part = 0;
+
+  // section을 forEach() 메서드로 하나씩 확인한다.
+  section.forEach((n) => {
+    // 현재 구역이 현재까지 칠한 구역보다 크다면
+    if (n > part) {
+      // 구역을 칠해주고 현재까지 칠한 구역을 업데이트 시켜준다.
+      part = n + m - 1;
+
+      // 페인트를 칠했으니 1증가 시킨다.
+      answer++;
+    }
+  });
+
+  return answer;
 }
