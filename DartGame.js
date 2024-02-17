e680d6cb20b86f8c125ea11756fb0b999252d051
prgmr99 <johnyeom24@naver.com>
2024-02-17 23:47:51 +0900

    [1차] 다트 게임

--- DartGame.js
+++ DartGame.js
@@ -1,22 +1,43 @@
 const str = "1S2D*3T";
-
-const test = "S";
-
 const strArr = str.split("");
-let num;
+const partition = [];
+
 let sum = 0;
+let starCnt = 0;
+let sharpCnt = 0;
+let str1 = "";
+const totalArr = [];
 
 for (let i = 0; i < strArr.length; i++) {
-  if (strArr[i] >= 0 && strArr[i] <= 9) {
-    num = Number(strArr[i]);
+  if (i !== 0 && strArr[i] <= 9 && strArr[i] >= 0) {
+    partition.push(str1);
+    str1 = "";
   }
-  if (strArr[i] === "S") {
-    sum += num;
-  }
-  if (strArr[i] === "D") {
-    sum += num * num;
-  }
-  if (strArr[i] === "T") {
-    sum += num * num * num;
-  }
+  str1 += strArr[i];
 }
+partition.push(str1);
+
+partition;
+
+for (let i = 0; i < partition.length; i++) {
+  const num = Number(partition[i][0]);
+  let total = 0;
+
+  if (partition[i].includes("S")) {
+    total += num;
+  } else if (partition[i].includes("D")) {
+    total += num * num;
+  } else if (partition[i].includes("T")) {
+    total += num * num * num;
+  }
+
+  if (partition[i].includes("*")) {
+    total *= 2;
+  } else if (partition[i].includes("#")) {
+    total *= -1;
+  }
+
+  totalArr.push(total);
+}
+
+totalArr;
