44876af6214766b41613aa7e65f159ec5e311564
prgmr99 <johnyeom24@naver.com>
2024-02-19 10:52:38 +0900

    [1차] 다트 게임

--- DartGame.js
+++ DartGame.js
@@ -28,3 +28,32 @@ function solution(dartResult) {
 
   return sum;
 }
+
+// 다른 사람 풀이
+function solution(dartResult) {
+  const bonus = { S: 1, D: 2, T: 3 },
+    options = { "*": 2, "#": -1, undefined: 1 };
+
+  let darts = dartResult.match(/\d.?\D/g);
+  // ? 없거나 최대 한 개만
+  // \d 숫자
+  // \D 숫자가 아닌 것
+  // . 모든 문자열(숫자, 한글, 영어, 특수문자, 공백 모두)
+  // g 문자열 내 모든 패턴 검색
+
+  for (let i = 0; i < darts.length; i++) {
+    let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/),
+      score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+    // (^\d{1,}): 시작 지점에 1회 이상의 숫자에 매칭
+    // (S|D|T): 'S', 'D', 또는 'T'에 매칭
+    // (\*|#)?: 옵션으로 '*' 또는 '#'에 0 또는 1회 매칭
+    // 이 정규표현식은 다트 결과에서 각 판의 점수, 보너스, 옵션을 추출합니다.
+    if (split[3] === "*" && darts[i - 1]) darts[i - 1] *= options["*"];
+
+    darts[i] = score;
+  }
+
+  return darts.reduce((a, b) => a + b);
+}
+
+solution("1S2D*3T");
