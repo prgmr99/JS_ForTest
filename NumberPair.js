148ca5dbf1922960154bbe9f0622307885ce1a0a
prgmr99 <johnyeom24@naver.com>
2024-02-23 11:06:06 +0900

    숫자 짝궁 - 시간초과 해결

--- NumberPair.js
+++ NumberPair.js
@@ -5,28 +5,41 @@ function solution(X, Y) {
   let yArr = Y.split("").map(Number);
   let temp = [...yArr];
 
+  const xCntArr = Array(10).fill(0);
+  const yCntArr = Array(10).fill(0);
+  const temp2 = Array(10).fill(0);
+
   for (let i = 0; i < xArr.length; i++) {
-    if (yArr.includes(xArr[i])) {
-      const index = yArr.indexOf(xArr[i]);
-      yArr.splice(index, 1);
+    xCntArr[xArr[i]]++;
+  }
+
+  for (let i = 0; i < yArr.length; i++) {
+    yCntArr[yArr[i]]++;
+  }
+
+  for (let i = 0; i < 10; i++) {
+    temp2[i] = xCntArr[i] > yCntArr[i] ? yCntArr[i] : xCntArr[i];
+  }
+
+  let str = "";
+  for (let i = 0; i < 10; i++) {
+    if (temp2[i] !== 0) {
+      const elem = i.toString();
+      str += elem.repeat(temp2[i]);
     }
   }
 
-  yArr.forEach((element) => {
-    const indexInTemp = temp.indexOf(element);
-    if (indexInTemp !== -1) {
-      temp.splice(indexInTemp, 1);
-    }
-  });
+  const answer = str
+    .split("")
+    .map(Number)
+    .sort((a, b) => b - a);
 
-  temp.sort((a, b) => b - a);
-
-  if (temp[0] === 0) {
+  if (answer[0] === 0) {
     result = "0";
-  } else if (temp.length === 0) {
+  } else if (answer.length === 0) {
     result = "-1";
   } else {
-    result = temp.join("");
+    result = answer.join("");
   }
 
   return result;
