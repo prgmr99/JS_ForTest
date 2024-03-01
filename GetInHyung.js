a22e9c75327be5e1123c3f9d7f749f4954cc736f
prgmr99 <johnyeom24@naver.com>
2024-03-01 23:27:02 +0900

    크레인 인형뽑기 게임 - 2차

--- GetInHyung.js
+++ GetInHyung.js
@@ -10,9 +10,9 @@ const moves = [1, 5, 3, 5, 1, 2, 1, 4];
 
 const stack = [];
 
-for (let k = 0; k < moves.length; k++) {
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
+first: for (let k = 0; k < moves.length; k++) {
+  second: for (let i = 0; i < board.length; i++) {
+    third: for (let j = 0; j < board[i].length; j++) {
       if (board[i][j] === 0) {
         continue;
       } else if (j !== moves[k] - 1) {
@@ -22,11 +22,14 @@ for (let k = 0; k < moves.length; k++) {
         j === moves[k] - 1 &&
         board[i][j] === 0
       ) {
-        break;
+        break second;
       } else if (j === moves[k] - 1 && board[i][j] !== 0) {
         stack.push(board[i][j]);
         board[i][j] = 0;
+        break second;
       }
     }
   }
 }
+
+stack;
