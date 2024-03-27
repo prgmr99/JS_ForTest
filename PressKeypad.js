6dba51378ae9b042e4a6e75afb9c0ed5a1be3828
prgmr99 <johnyeom24@naver.com>
2024-03-27 17:18:54 +0900

    키패드 누르기

---
+++ PressKeypad.js
@@ -0,0 +1,60 @@
+function solution(numbers, hand) {
+  let result = "";
+
+  const keypad = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+    0: [3, 1],
+    "*": [3, 0],
+    "#": [3, 2],
+  };
+
+  const leftArr = [1, 4, 7];
+  const rightArr = [3, 6, 9];
+  let currentLeft = "*";
+  let currentRight = "#";
+  let distLeft = -1;
+  let distRight = -1;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (leftArr.includes(numbers[i])) {
+      currentLeft = numbers[i];
+      result += "L";
+    } else if (rightArr.includes(numbers[i])) {
+      currentRight = numbers[i];
+      result += "R";
+    } else {
+      const num = keypad[numbers[i]];
+      const leftNum = keypad[currentLeft];
+      const rightNum = keypad[currentRight];
+      distLeft = Math.abs(leftNum[0] - num[0]) + Math.abs(leftNum[1] - num[1]);
+      distRight =
+        Math.abs(rightNum[0] - num[0]) + Math.abs(rightNum[1] - num[1]);
+
+      if (distLeft === distRight) {
+        if (hand === "right") {
+          result += "R";
+          currentRight = numbers[i];
+        } else if (hand === "left") {
+          result += "L";
+          currentLeft = numbers[i];
+        }
+      } else if (distLeft > distRight) {
+        result += "R";
+        currentRight = numbers[i];
+      } else if (distLeft < distRight) {
+        result += "L";
+        currentLeft = numbers[i];
+      }
+    }
+  }
+
+  return result;
+}
