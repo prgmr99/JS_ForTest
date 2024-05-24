356e7b1a3ad5c69db45b315e7b8f88d3bbcaae06
prgmr99 <johnyeom24@naver.com>
2024-05-24 13:37:21 +0900

    공원산책 - 2차

--- GardenJog.js
+++ GardenJog.js
@@ -1,96 +1,39 @@
-const park = ["SOO", "OXX", "OOO"];
-const routes = ["E 2", "S 2", "W 1"];
-// result = [2,1]
-let [x, y] = [0, 0];
-let current = [x, y];
-const obstacles = {};
+const park = ["OSO","OOO","OXO","OOO"];
+const routes = ["E 2","S 3","W 1"];
 
-// park 좌표로 변환
-const parkCord = park.map((e) => e.split(""));
+const N = park.length;
+const M = park[0].length;
 
-// 시작 지점, 장애물 찾는 코드
-for (let i = 0; i < parkCord.length; i++) {
-  for (let j = 0; j < parkCord[i].length; j++) {
-    if (parkCord[i][j] === "S") {
-      [x, y] = [i, j];
-    }
-    if (parkCord[i][j] === "X") {
-      const key = i;
-      const value = j;
-
-      if (obstacles[key]) {
-        // 키가 이미 존재하면 값 배열에 추가
-        obstacles[key].push(value);
-      } else {
-        // 키가 없으면 새 배열로 추가
-        obstacles[key] = [value];
-      }
-    }
+// 시작 좌표 설정
+let start;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (park[i][j] == "S") start = [i, j];
   }
 }
-current[1];
-obstacles[current[1]];
-
-// 경로 이동
-outerloop: for (let i = 0; i < routes.length; i++) {
-  let previous = current;
-  if (routes[i][0] === "E") {
-    for (let j = 0; j < +routes[i][2]; j++) {
-      if (current[1] > park[0].length - 1) {
-        // 이탈 조건 추가
-        currrent = previous;
-        continue outerloop;
-      }
-      if (obstacles[current[0]]?.includes(current[1])) {
-        currrent = previous;
-        continue outerloop;
-      } else {
-        current[1] += 1;
-      }
-    }
-  } else if (routes[i][0] === "W") {
-    for (let j = 0; j < +routes[i][2]; j++) {
-      if (current[1] < 0) {
-        // 이탈 조건 추가
-        currrent = previous;
-        continue outerloop;
-      }
-      if (obstacles[current[0]]?.includes(current[1])) {
-        currrent = previous;
-        continue outerloop;
-      } else {
-        current[1] -= 1;
-      }
-    }
-  } else if (routes[i][0] === "S") {
-    for (let j = 0; j < +routes[i][2]; j++) {
-      if (current[0] > park.length - 1) {
-        // 이탈 조건 추가
-        currrent = previous;
-        continue outerloop;
-      }
-      if (obstacles[current[0]]?.includes(current[1])) {
-        currrent = previous;
-        continue outerloop;
-      } else {
-        current[0] += 1;
-      }
-    }
-  } else if (routes[i][0] === "N") {
-    for (let j = 0; j < +routes[i][2]; j++) {
-      if (current[0] < 0) {
-        // 이탈 조건 추가
-        currrent = previous;
-        continue outerloop;
-      }
-      if (obstacles[current[0]]?.includes(current[1])) {
-        currrent = previous;
-        continue outerloop;
-      } else {
-        current[0] -= 1;
-      }
-    }
+// 방향 객체
+const directions = {
+  E: [0, 1],
+  W: [0, -1],
+  S: [1, 0],
+  N: [-1, 0],
+};
+// 주어진 이동정보 배열에 담기
+for (const route of routes) {
+  const [dir, distanceStr] = route.split(" ");
+  let distance = parseInt(distanceStr);
+  let [nx, ny] = start;
+  // 주어진 걸음수 만큼 한칸씩 이동
+  let step = 0;
+  while (step < distance) {
+    nx += directions[dir][0];
+    ny += directions[dir][1];
+    // 만약 밖에 나가게 되거나 X를 만나게 된다면 종료
+    if (nx < 0 || N <= nx || ny < 0 || M <= ny || park[nx][ny] === "X") break;
+    step++;
   }
+  // 원하는 걸음수를 채우면 start는 마지막 위치로 바꿈
+  if (step === distance) start = [nx, ny];
 }
 
-current;
+start;
