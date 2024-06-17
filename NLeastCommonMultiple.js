8b540bc4bd9741702adb6fea5a7bc37ccd53a2eb
prgmr99 <johnyeom24@naver.com>
2024-06-17 09:15:49 +0900

    N개의 최소공배수

---
+++ NLeastCommonMultiple.js
@@ -0,0 +1,16 @@
+function gcd(a, b) {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function solution(arr) {
+  return arr.reduce((acc, num) => lcm(acc, num), 1);
+}
