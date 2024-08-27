7a4943ef882c26278870b839fe1169e796619d8c
prgmr99 <johnyeom24@naver.com>
2024-08-27 22:11:05 +0900

    괄호 회전하기

---
+++ TurningBrackets.js
@@ -0,0 +1,39 @@
+function solution(s) {
+  let count = 0;
+  const arr = s.split("");
+
+  const validationCheck = (arr) => {
+    const stack = [];
+    const openArr = ["[", "(", "{"];
+    const closeArr = ["]", ")", "}"];
+
+    if (closeArr.includes(arr[0])) return false;
+
+    for (let e of arr) {
+      if (openArr.includes(e)) {
+        stack.push(e);
+      } else if (closeArr.includes(e)) {
+        if (stack.length === 0) return false;
+
+        const lastOpen = stack.pop();
+        if (
+          (e === ")" && lastOpen !== "(") ||
+          (e === "]" && lastOpen !== "[") ||
+          (e === "}" && lastOpen !== "{")
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    arr.push(arr.shift());
+    if (validationCheck(arr)) {
+      count++;
+    }
+  }
+  return count;
+}
