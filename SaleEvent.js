e24a0ee20acc568527d0548af2750ea28f3fa93f
prgmr99 <johnyeom24@naver.com>
2024-08-28 21:46:09 +0900

    할인 행사 (개선 코드 추가)

--- SaleEvent.js
+++ SaleEvent.js
@@ -33,3 +33,50 @@ function solution(want, number, discount) {
 
   return count;
 }
+
+// 개선한 코드
+function solution(want, number, discount) {
+  let count = 0;
+  const obj = {};
+
+  // 초기 obj 설정
+  for (let i = 0; i < want.length; i++) {
+    obj[want[i]] = number[i];
+  }
+
+  // 초기 윈도우 설정 (첫 10일)
+  for (let i = 0; i < 10; i++) {
+    if (obj[discount[i]] !== undefined) {
+      obj[discount[i]]--;
+    }
+  }
+
+  // 윈도우의 모든 항목이 충족되었는지 확인
+  const isAllZero = () => Object.values(obj).every((val) => val === 0);
+
+  if (isAllZero()) {
+    count++;
+  }
+
+  // 슬라이딩 윈도우 적용
+  for (let i = 10; i < discount.length; i++) {
+    // 이전 윈도우의 첫 번째 항목 복원
+    const firstItem = discount[i - 10];
+    if (obj[firstItem] !== undefined) {
+      obj[firstItem]++;
+    }
+
+    // 새로운 윈도우의 마지막 항목 처리
+    const newItem = discount[i];
+    if (obj[newItem] !== undefined) {
+      obj[newItem]--;
+    }
+
+    // 윈도우가 조건을 만족하는지 확인
+    if (isAllZero()) {
+      count++;
+    }
+  }
+
+  return count;
+}
