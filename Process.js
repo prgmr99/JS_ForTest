3dd5fd9270995131dec380d85ed3e828b3c5cde2
prgmr99 <johnyeom24@naver.com>
2024-09-26 22:16:15 +0900

    프로세스 - 2차

--- Process.js
+++ Process.js
@@ -27,3 +27,25 @@ function solution(priorities, location) {
   }
   return answer;
 }
+
+// 2차
+
+function solution(priorities, location) {
+  let arr = priorities.map((priority, index) => ({ priority, index }));
+  let answer = 0;
+
+  while (arr.length > 0) {
+    const process = arr.shift();
+
+    if (arr.some((doc) => doc.priority > process.priority)) {
+      arr.push(process); // 우선순위가 높은 문서가 있으면 뒤로 보냄
+    } else {
+      answer += 1; // 현재 문서를 출력함
+
+      if (process.index === location) {
+        return answer; // 찾던 문서라면 출력 순서 반환
+      }
+    }
+  }
+  return answer;
+}
