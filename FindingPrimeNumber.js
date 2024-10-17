a48e7b21e2880b0d289fec471a986f0c0253cd5c
prgmr99 <johnyeom24@naver.com>
2024-10-17 20:55:08 +0900

    소수 찾기 - 완전탐색

--- FindingPrimeNumber.js
+++ FindingPrimeNumber.js
@@ -30,3 +30,37 @@ function solution(numbers) {
 
   return primeArr.length;
 }
+
+// 2차 풀이
+function solution(numbers) {
+  const numArr = numbers.split("");
+  const set = new Set();
+
+  // 소수인지 확인하는 로직
+  function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i < Math.sqrt(num); i++) {
+      if (num % 2 === 0) return false;
+    }
+    return true;
+  }
+
+  // 돌아가면서 숫자 경우를 생성하는 로직
+  function makeNum(number, used) {
+    if (number.length > 0) set.add(Number(number));
+
+    for (let i = 0; i < numArr.length; i++) {
+      if (!used[i]) {
+        const newUsed = [...used];
+        newUsed[i] = true;
+        makeNum(number + numArr[i], newUsed);
+      }
+    }
+  }
+
+  makeNum("", Array(numArr.length).fill(false));
+
+  const primeArr = Array.from(set).filter(isPrime);
+
+  return primeArr.length;
+}
