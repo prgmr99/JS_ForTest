8be54480d51f54800657557d345d68034c5fb041
Yeom <yeom@192.168.nate.com>
2025-01-15 23:24:09 +0900

    택배상자 - 1차

---
+++ Box.js
@@ -0,0 +1,25 @@
+// order = [4,3,1,2,5]
+// result = 2
+
+// order.length <= 1,000,000
+// 즉, 이중반복문은 안된다.
+
+function solution(order) {
+    let answer = 0;
+    const subContainer = [];
+
+    for(let i = 0;i < order.length;i++) {
+        if(order[i] !== i + 1) {
+            subContainer.push(order[i]);
+        } else if(order[i] === i + 1) {
+            answer++;
+        } else if(subContainer[subContainer.length - 1] === i + 1) {
+            subContainer.pop();
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution([4,3,1,2,5])); // 2
